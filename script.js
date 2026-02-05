fa6610677f85cac068e57fb320327749fe5566c3
safelife1108 <kumarrohit84333@gmail.com>
2026-02-05 22:59:50 +0530

    Remove revealOnScroll function and event listener

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const reveals = document.querySelectorAll(".reveal");
 
-function revealOnScroll() {
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const elementTop = reveals[i].getBoundingClientRect().top;
-    const elementVisible = 100;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", revealOnScroll);
-revealOnScroll();
