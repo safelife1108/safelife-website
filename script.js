c201ab702ae021b84556ac8861a2e1e2cb79b803
safelife1108 <kumarrohit84333@gmail.com>
2026-02-14 12:11:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-// Scroll progress bar
-window.addEventListener("scroll", () => {
-  const scrollTop = document.documentElement.scrollTop;
-  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const progress = (scrollTop / scrollHeight) * 100;
-  document.getElementById("scrollProgress").style.width = progress + "%";
-});
-
-// Reveal on scroll
-const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const elementTop = reveals[i].getBoundingClientRect().top;
-    const elementVisible = 100;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    }
-  }
+function showConfirmation(e) {
+  e.preventDefault();
+  alert("Thank you! Your booking request has been submitted. We will contact you soon.");
 }
-
-window.addEventListener("scroll", revealOnScroll);
-revealOnScroll();
-
